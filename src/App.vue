a71dfc6f0fcc509c22f95838be4f22c3933a415a
Alexey Aleev <aleevas@gmail.com>
2018-11-21 17:48:40 +0200

    Added some styles

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,5 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
     <Skills/>
   </div>
 </template>
@@ -18,12 +17,19 @@ export default {
 </script>
 
 <style>
+body {
+  background-color: #EEEEEE;
+  font-family: 'Montserrat', sans-serif;
+  display: grid;
+  grid-template-rows: auto;
+  justify-items: center;
+  align-items: center;
+}
+body, html {
+  margin: 0;
+  height: 100%;
+}
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+    width: 50%;
 }
 </style>
--- src/components/Skills.vue
+++ src/components/Skills.vue
@@ -1,12 +1,17 @@
 <template>
-  <div class="hello">
-    <h1>{{name}}</h1>
-    <ul>
-     <li v-for="(value, index) in skills" :key='index'>
-       {{index}} . {{value.skill}}
-     </li>
-    </ul>
-    <button v-on:click="ChangeName" v-bind:disabled="btnState">{{btnName}}</button>
+  <div class="container">
+    <div class="holder">
+      <h1>{{title}}</h1>
+      <ul>
+        <li v-for="(value, index) in skills" :key='index'>
+          {{index}} . {{value.skill}}
+        </li>
+      </ul>
+      <button v-on:click="ChangeName" v-bind:disabled="btnState">{{btnName}}</button>
+      <!-- <div v-bind:style="{backgroundColor:bgColor,with: bgWith, height: bgHeight, marginTop: bgMarginTop}"> -->
+          <p>These are the skills that you possess.</p>
+      <!-- </div> -->
+      </div>
   </div>
 </template>
 
@@ -15,13 +20,17 @@ export default {
   name: 'HelloWorld',
   data() {
     return {
-      name: "First name",
+      title: "My title",
       btnName: "Change",
       btnState: true,
       skills: [
         {'skill': 'First skill'},
         {'skill': 'Second skill'}
-      ]
+      ],
+      bgColor: 'lightgrey',
+      bgWith: '100%',
+      bgHeight: '25px',
+      bgMarginTop: '25px',
     }
   },
   props: {
